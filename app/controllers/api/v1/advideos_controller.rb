7624e032b316be1eb0859cbce23a5af6063ff89c
Nirmal Ch. Hota <nirmalh@Nirmals-MacBook-Air.local>
2016-04-14 12:19:36 +0530

    added the video listing

--- app/controllers/api/v1/advideos_controller.rb
+++ app/controllers/api/v1/advideos_controller.rb
@@ -4,11 +4,24 @@ class Api::V1::AdvideosController < ApplicationController
      render json: @advideo
   end
 
-  def display_videos
-     @advideo = Advideo.new    
-     @advideo.id = 60
-     @advideo.video_url = "http://cdn.reachtv.co/ad_videos/60/100_0101.mp4"
-     @advideo.hls_url = "http://hls.reachtv.co/ad_videos/60/100_0101.mp4.m3u8"    
+  def display_videos     
+     if params[:no_of_videos]
+       @advideo = Advideo.last(params[:no_of_videos])
+       @advideo.hls_url  = @advideo.video_url
+       @advideo.hls_url  = @advideo.hls_url.sub! 'https://reachtv-1238.storage.googleapis.com', 'http://hls.reachtv.co'
+       @advideo.hls_url  = @advideo.hls_url.sub! 'http://cdn.reachtv.co', 'http://hls.reachtv.co'
+       @advideo.hls_url = @advideo.hls_url + ".m3u8"       
+     else
+       @advideo = Advideo.last
+       @advideo.hls_url  = @advideo.video_url
+       @advideo.hls_url  = @advideo.hls_url.sub! 'https://reachtv-1238.storage.googleapis.com', 'http://hls.reachtv.co'
+       @advideo.hls_url  = @advideo.hls_url.sub! 'http://cdn.reachtv.co', 'http://hls.reachtv.co'       
+       @advideo.hls_url = @advideo.hls_url + ".m3u8"
+     end 
+     # @advideo = Advideo.new    
+     # @advideo.id = 60
+     # @advideo.video_url = "http://cdn.reachtv.co/ad_videos/60/100_0101.mp4"
+     # @advideo.hls_url = "http://hls.reachtv.co/ad_videos/60/100_0101.mp4.m3u8"    
      render json: @advideo
   end 
 end
