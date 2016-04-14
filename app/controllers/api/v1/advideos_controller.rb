c6085506461e23740d3f435b0553cc264bb7f92a
Nirmal Ch. Hota <nirmalh@Nirmals-MacBook-Air.local>
2016-04-14 13:24:17 +0530

    added the video listing

--- app/controllers/api/v1/advideos_controller.rb
+++ app/controllers/api/v1/advideos_controller.rb
@@ -7,15 +7,19 @@ class Api::V1::AdvideosController < ApplicationController
   def display_videos     
      if params.has_key?(:no_of_videos)
        @advideo = Advideo.last(params[:no_of_videos])
+       @advideo.each do |video|
+         v_url = video.video_url
+         v_url.sub 'http://cdn.reachtv.co', 'http://hls.reachtv.co'
+         v_url = v_url + ".m3u8"
+         video.hls_url = v_url   
+       end               
      else
        @advideo = Advideo.last
+         v_url = @advideo.video_url
+         v_url.sub! 'http://cdn.reachtv.co', 'http://hls.reachtv.co'
+         v_url = v_url + ".m3u8"
+         @advideo.hls_url = v_url              
      end
-     @advideo.each do |i|
-       @advideo[i]["hls_url"] = @advideo[i]["video_url"]
-       @advideo[i]["hls_url"] = @advideo[i]["hls_url"].sub! 'https://reachtv-1238.storage.googleapis.com', 'http://hls.reachtv.co'
-       @advideo[i]["hls_url"] = @advideo[i]["hls_url"].sub! 'http://cdn.reachtv.co', 'http://hls.reachtv.co'
-       @advideo[i]["hls_url"] = @advideo[i]["hls_url"] + ".m3u8"              
-     end 
      # @advideo = Advideo.new    
      # @advideo.id = 60
      # @advideo.video_url = "http://cdn.reachtv.co/ad_videos/60/100_0101.mp4"
